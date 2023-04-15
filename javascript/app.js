184bd20327311c6033e14802dcfc022934b23a48
Shashijha1617 <130572406+Shashijha1617@users.noreply.github.com>
2023-04-15 12:03:35 +0530

    update

--- javascript/app.js
+++ javascript/app.js
@@ -2,3 +2,13 @@ function extendNav(){
     const navlist = document.querySelector('.nav-toggle');
     navlist.classList.toggle("Show"); 
 }
+
+window.addEventListener('scroll', function(){
+
+    let navbar = document.querySelector('.navbar');
+
+    if(window.pageYOffset >= 74){
+        navbar.classList.add('stickey-nav');
+    }else 
+        navbar.classList.remove('stickey-nav');
+})
